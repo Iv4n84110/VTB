b5e54717e67c172b834250cc04f1c27249abe28c
Iv4n84110 <vanka3087@gmail.com>
2020-10-12 22:34:17 +0300

    fix sections width

--- src/pages/User/Content/Sections/Users/index.js
+++ src/pages/User/Content/Sections/Users/index.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import classes from './styles.css'
 
 const Users = () => {
+
+
+    const [users, setUsers] = useState(null)
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/1b0f1a45-c8dc-441b-b566-00a28a3fd006")
+            .then((res) => res.json())
+            .then((res) => setUsers(res.users))
+    }, [])
+
+
+    {
+        console.log(users)
+    }
+
+    const tabs = users ? users.map(user => {
+        return <div className={classes.Tab}>{user.login}</div>
+        })
+        : null
+
     return (
-        <div>Users</div>
+        <div className={classes.TabsWrapper}>
+            {tabs}
+        </div>
     )
 
 }
