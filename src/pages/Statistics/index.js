075238f5f19ca7cc04b64908d7acbf928622b7c1
Elina <ella-abella@mail.ru>
2020-10-20 12:11:19 +0300

    chart

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -3,6 +3,7 @@ import PageLayout from "../../shared/PageLayout";
 import SignIn from "../Main/SignIn/index.js";
 import Modal from "../../shared/Modal";
 import { Redirect } from "react-router-dom";
+import {Bar} from 'react-chartjs-2';
 
 const Statistics = () => {
     const [modal, setModal] = useState(false);
@@ -22,6 +23,35 @@ const Statistics = () => {
         console.log('click');
     }
 
+    const users = ["January", "February", "March", "April", "May", "June", "July", "July"];
+
+    const colors = [
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(43, 0, 119, 0.6)',
+        'rgba(96, 0, 120, 0.6)',
+        'rgba(120, 0, 80, 0.6)',
+        'rgba(40, 168, 45, 0.6)',
+    ];
+
+    const getRandomColor = () => (colors[Math.floor(Math.random() * colors.length)]);
+
+    const backgroundColors = users.map(() => getRandomColor());
+
+    const data = {
+        labels: users,
+        datasets: [{
+            label: "User's activity",
+            backgroundColor: backgroundColors,
+            data: [40, 10, 5, 2, 20, 30, 45, 38],
+        }]
+    }
+
     return (
         openedMain ? <Redirect to="/" /> :
         <div>
@@ -29,7 +59,7 @@ const Statistics = () => {
             <Modal show={modal}  modalClosed={closeModal}>
                 <SignIn modalClosed={closeModal}/>
             </Modal>
-            <span>statistics</span>
+            < Bar data={data} height={150} />
             </PageLayout>
         </div>
     )
